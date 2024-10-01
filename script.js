64eaa78ffe4d28dbfddc98e125ed189034babe9e
rodrigosantossilv <rodrigosantossilva1705@gmail.com>
2024-10-01 19:56:00 -0300

    caminhoes

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-var menuItem = document.querySelectorAll('.item-menu')
+var menuItem = document.querySelectorAll('.item-menu');
+var caminhõesContainer = document.getElementById('caminhoes');
 
-function selectLink(){
-    menuItem.forEach((item)=>
-        item.classList.remove('ativo')
-    )
-    this.classList.add('ativo')
+function selectLink() {
+    menuItem.forEach((item) => {
+        item.classList.remove('ativo');
+    });
+
+    this.classList.add('ativo');
+
+    // Mostra ou esconde o container de caminhões
+    if (this.querySelector('.txt-link').textContent === "Caminhoes") {
+        caminhõesContainer.style.display = 'flex'; // Mostra os cards
+    } else {
+        caminhõesContainer.style.display = 'none'; // Esconde os cards
+    }
 }
 
-menuItem.forEach((item)=>
-    item.addEventListener('click',selectLink)
-)
+menuItem.forEach((item) => item.addEventListener('click', selectLink));
 
-//expandir menu
-var btnExp = document.querySelector('#btn-exp')
-var menuSide = document.querySelector('.menu-lateral')
+// Expandir menu
+var btnExp = document.querySelector('#btn-exp');
+var menuSide = document.querySelector('.menu-lateral');
 
-btnExp.addEventListener('click', function(){
-    menuSide.classList.toggle('expandir')
-})
+btnExp.addEventListener('click', function() {
+    menuSide.classList.toggle('expandir');
+});
